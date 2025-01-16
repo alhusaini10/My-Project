3ffabc5a719c934e0b29f8446ff3df318819e176
alhusaini10 <m.alhusainii18@gmail.com>
2025-01-16 10:18:44 +0700

    3 Project

---
+++ siswa.js
@@ -0,0 +1,18 @@
+const bazma = {
+    proker: 'PPLG',
+    konsentrasi: 'SIJA',
+    program: '4 tahun',
+    kurikulum: 'Merdeka',
+};
+
+console.log(`halo,jurusan saya ${bazma.konsentrasi} sedang ujian praktek ${bazma.proker} bersama Pak Dzikri`);
+console.log(`saya angkatan ke ${bazma.program} di bazma`);
+
+const saya = {
+    nama: 'M.Nashiruddin Al Husaini',
+    asal: 'Bogor,Kec.Cileungsi-Jonggol',
+    umur: 16,
+    kelas: 10,
+};
+
+console.log(`halo,nama saya ${saya.nama},saya berasal dari ${saya.asal} ,umur saya ${saya.umur} tahun,saya kelas ${saya.kelas} di SMK TI BAZMA`);
