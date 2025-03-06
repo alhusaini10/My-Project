f2856735431b356450d8581f55f14d181b2a7073
alhusaini10 <m.alhusainii18@gmail.com>
2025-03-06 10:57:34 +0700

    Add files via upload

--- fuction.js
+++ fuction.js
@@ -1,15 +1,31 @@
-function inifungsi() {
-    // function scope
-    var nama = 'Nashir';
-}
-// global scope
-console.log(nama);
+// Some code
+function train(brand, color, maxSpeed, chassisNumber) {
+    this.brand = brand;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+    this.chassisNumber = chassisNumber;
+  }
+   
+  train.prototype.drive = function() {
+    console.log(`${this.brand} ${this.color} is driving`);
+  }
+   
+  train.prototype.reverse = function() {
+    console.log(`${this.brand} ${this.color} is reversing`);
+  }
+   
+  train.prototype.turn = function() {
+    console.log(`${this.brand} ${this.color} is turning`);
+  }
 
-// global scope
-var nama = 'nashir';
-if (true) {
-    // blockscope
-    var nama= Al
-}
-//global scope
-console.log(nama); // Al
+const train1 = new train('Alstom', 'White', 350, 'TGV');
+const train2 = new train('KAI', 'White', 300, 'TGV');
+const train3 = new train('Network Rail', 'White', 300, 'TGV');
+
+console.log(train1);
+console.log(train2);
+console.log(train3);
+
+train1.drive();
+train2.reverse();
+train3.turn();
---
+++ oop.js
@@ -0,0 +1,80 @@
+// Some code
+const train = {
+    // properties
+    brand: 'Alstom',
+    color: 'White',
+    maxSpeed: 350,
+    chassisNumber: 'TGV',
+    // methods
+    drive: () => {
+      console.log('driving');
+    },
+    reverse: () => {
+      console.log('reversing');
+    },
+    stop: () => {
+      console.log('stoping');
+    }
+  }
+   
+  console.log(train.brand); 
+  console.log(train.color); 
+  console.log(train.maxSpeed); 
+  console.log(train.chassisNumber); 
+  train.drive(); 
+  train.reverse();
+  train.stop(); 
+
+
+  const mytrain = {
+    // properties
+    brand: 'KAI',
+    color: 'White',
+    maxSpeed: 300,
+    chassisNumber: 'TGV',
+    // methods
+    drive: () => {
+      console.log('driving');
+    },
+    reverse: () => {
+      console.log('reversing');
+    },
+    stop: () => {
+      console.log('stoping');
+    }
+  }
+   
+  console.log(mytrain.brand); 
+  console.log(mytrain.color); 
+  console.log(mytrain.maxSpeed); 
+  console.log(mytrain.chassisNumber); 
+  mytrain.drive(); 
+  mytrain.reverse();
+  mytrain.stop(); 
+
+
+const yourtrain = {
+    // properties
+    brand: 'Alstom',
+    color: 'White',
+    maxSpeed: 350,
+    chassisNumber: 'TGV',
+    // methods
+    drive: () => {
+      console.log('driving');
+    },
+    reverse: () => {
+      console.log('reversing');
+    },
+    stop: () => {
+      console.log('stoping');
+    }
+  }
+   
+  console.log(yourtrain.brand); 
+  console.log(yourtrain.color); 
+  console.log(yourtrain.maxSpeed); 
+  console.log(yourtrain.chassisNumber); 
+  yourtrain.drive(); 
+  yourtrain.reverse();
+  yourtrain.stop(); 
